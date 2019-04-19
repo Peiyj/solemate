39897f6e69594fedaff64011564cea6e05e62f83
Steven Tran <steventraaan@gmail.com>
2019-04-18 18:42:23 -0700

    created project

---
+++ Solemate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solemate
+//
+//  Created by Steven Tran on 4/18/19.
+//  Copyright © 2019 SolemateInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
