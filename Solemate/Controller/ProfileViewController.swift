3ddac6a3d625dd8877700003a214ff6f08a478b1
Steven Tran <steventraaan@gmail.com>
2019-05-15 20:42:14 -0700

    successfully pulled and displayed rehab session data on profile page

--- Solemate/Controller/ProfileViewController.swift
+++ Solemate/Controller/ProfileViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 import FirebaseAuth
 import FirebaseFirestore
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var firstNameLabel: UILabel!
     @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var dobLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
-    @IBOutlet weak var goalLabel: UILabel!
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var surgeryDateLabel: UILabel!
-    @IBOutlet weak var rehabTimeLabel: UILabel!
     @IBOutlet weak var signOutButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     
     var window: UIWindow?
+    var rehabSessions = [[String]]()
     
     let db = Firestore.firestore()
     
@@ -52,12 +52,63 @@ class ProfileViewController: UIViewController {
                 self.conditionLabel.text = document["condition"] as? String
                 self.surgeryDateLabel.text = document["surgeryDate"] as? String
                 self.nameLabel.text = (self.firstNameLabel.text!) + "!"
+                // Populating RehabSessions Array
+                if let session1 = document["session1"] as? [String] {
+                    print("Enter session1")
+                    self.rehabSessions.append(document["session1"] as! [String])
+                    self.tableView.reloadData()
+                }
+                if let session2 = document["session2"] as? [String] {
+                    print("Enter session2")
+                    self.rehabSessions.append(document["session2"] as! [String])
+                    self.tableView.reloadData()
+                }
+                if let session3 = document["session3"] as? [String] {
+                    print("Enter session3")
+                    self.rehabSessions.append(document["session3"] as! [String])
+                    self.tableView.reloadData()
+                }
+                if let session4 = document["session4"] as? [String] {
+                    print("Enter session4")
+                    self.rehabSessions.append(document["session4"] as! [String])
+                    self.tableView.reloadData()
+                }
             } else {
                 print("Document does not exist")
             }
         }
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        print(rehabSessions)
+    } // end viewDidLoad
+    
+    /* TABLEVIEW CODE */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rehabSessions.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SessionCell") as! SessionCell
+        
+        let session = rehabSessions[indexPath.row]
+        let weight = session[1] + " % Body Weight"
+        let weeks = session[0] + " Weeks"
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 70
+        
+        cell.weekLabel!.text = weeks
+        cell.weightLabel!.text = weight
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    /* END TABLEVIEW CODE */
+    
     
     @IBAction func signOutPressed(_ sender: Any) {
         let firebaseAuth = Auth.auth()
